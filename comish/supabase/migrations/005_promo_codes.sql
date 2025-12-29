f5d23fc126245cef8d5dfbaef73ca53b7c6cf22c
AmericanSurvivalst <americansurvivalist.us@gmail.com>
2025-12-29 11:36:10 -0500

    Delete comish directory

--- comish/supabase/migrations/005_promo_codes.sql
+++
@@ -1,5 +0,0 @@
--- Add promo_code column to profiles table
-ALTER TABLE profiles ADD COLUMN IF NOT EXISTS promo_code TEXT;
-
--- Create index for promo code lookups
-CREATE INDEX IF NOT EXISTS idx_profiles_promo_code ON profiles(promo_code);
